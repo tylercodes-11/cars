46525630550e0c2bcacfc75e0b9d6ed085f83c31
Ty Reid <trawx11@gmail.com>
2023-06-11 11:36:45 -0400

    index store updates

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+    carsReducer
+    ,addCar
+    ,removeCar
+    ,changeSearchTerm
+} from './slices/carsSlice';
+import { formReducer, changeName, changeCost } from './slices';
+
+const store = configureStore({
+    reducer: {
+        cars: carsReducer,
+        form: formReducer
+    }
+});
+
+export {
+    store,
+    changeName,
+    changeCost,
+    addCar,
+    removeCar,
+    changeSearchTerm
+}
